a85909ac2139331fb923f9169c8e6b9d982e9292
ggtrd <ggontard@L1801-00350>
2024-07-23 13:05:25 +0200

    Directories & files list check is working

--- commands/tests.sh
+++ commands/tests.sh
@@ -25,65 +25,134 @@
 
 
 
-# Overwrite relatives paths with their absolutes values to ensure right tests results
-file_config=$dir_config"/"$NAME_LOWERCASE"_config"
-file_systemd_update="$dir_systemd/$NAME_LOWERCASE-updates"
+# General information
+# This file permit to test if the current installation is working or not.
+# It must be used within the main CLI file since most of the variables used here are declared in the main CLI file.
 
-# Creating variables for systemd timers since they are in a list in the main CLI file
-file_systemd_timers_update="$dir_systemd/$file_systemd_update.timer"
+
+
+dir_bin="/usr/local/sbin"
+dir_src="/usr/local/src/$NAME_LOWERCASE"
+dir_systemd="/lib/systemd/system"
+dir_config="/etc/$NAME_LOWERCASE"
+if [[ $(exists_command "pkg-config") = "exists" ]]; then
+	dir_autocompletion="$(pkg-config --variable=compatdir bash-completion)"
+else
+	dir_autocompletion="/etc/bash_completion.d"
+fi
+
+
+# "Core"	-> means that the CLI can't be installed, uninstalled or updated without it.
+# "Other"	-> means that the CLI can be installed, uninstalled or updated without it.
+# (the update process must handle a clean installation by deleting or creating right files at right places).
+directories_core=(
+	$dir_bin
+)
+directories_other=(
+	$dir_src
+	$dir_systemd
+	$dir_config
+	$dir_autocompletion
+)
+files_core=(
+	"$dir_bin/$NAME_LOWERCASE"
+	"$dir_bin/bp"
+)
+files_other=(
+	"$dir_autocompletion/$NAME_LOWERCASE"
+	"$dir_systemd/$NAME_LOWERCASE-updates.service"
+	"$dir_systemd/$NAME_LOWERCASE-updates.timer"
+	"$dir_config/.current_publication"
+	"$dir_config/"$NAME_LOWERCASE"_config"
+)
+
+
+
 
 # Permit to check if files exist or not
 check_files() {
 
-	local number_exists=0
-	local number_notfound=0
+	local number_core_found=0
+	local number_core_notfound=0
 
-	local directories=(
-		$dir_bin
-		$dir_src
-		$dir_systemd
-		$dir_config
-		$dir_autocompletion
-	)
-
-	local files=(
-		$file_main
-		$file_main_alias
-		$file_autocompletion
-		$file_systemd_update
-		$file_systemd_timers_update
-		$file_current_publication
-		$file_config
-	)
-
-
-	for directory in "${directories[@]}"; do
+	local number_other_found=0
+	local number_other_notfound=0
+	
+	# Core directories
+	for directory in "${directories_core[@]}"; do
 		if [ -d $directory ]; then
-			echo "[TEST] EXISTS		-> $directory"
-			number_exists=$(number_exists + 1)
+			echo "[dir]  Found		-> $directory"
+			number_core_found=$((number_core_found+1))
 		else
-			echo "[TEST] NOT FOUND	-> $directory"
-			number_notfound=$(number_notfound + 1)
+			echo "[dir]  Not found	-> $directory"
+			number_core_notfound=$((number_core_notfound+1))
+		fi
+	done
+	
+	# Other directories
+	for directory in "${directories_other[@]}"; do
+		if [ -d $directory ]; then
+			echo "[dir]  Found		-> $directory"
+			number_other_found=$((number_other_found+1))
+		else
+			echo "[dir]  Not found	-> $directory"
+			number_other_notfound=$((number_other_notfound+1))
 		fi
 	done
 
-
-	for file in "${files[@]}"; do
+	# Core files
+	for file in "${files_core[@]}"; do
 		if [ -f $file ]; then
-			echo "[TEST] EXISTS		-> $file"
-			number_exists=$(number_exists + 1)
+			echo "[file] Found		-> $file"
+			number_core_found=$((number_core_found+1))
 		else
-			echo "[TEST] NOT FOUND	-> $file"
-			number_notfound=$(number_notfound + 1)
+			echo "[file] Not found	-> $file"
+			number_core_notfound=$((number_core_notfound+1))
 		fi
 	done
+
+	# Other files
+	for file in "${files_other[@]}"; do
+		if [ -f $file ]; then
+			echo "[file] Found		-> $file"
+			number_other_found=$((number_other_found+1))
+		else
+			echo "[file] Not found	-> $file"
+			number_other_notfound=$((number_other_notfound+1))
+		fi
+	done
+
+
+	number_core_total=$((${#directories_core[@]}+${#files_core[@]}))
+	number_other_total=$((${#directories_other[@]}+${#files_other[@]}))
 
 
 	echo ""
-	echo "Exist: $number_exists | Not found: $number_notfound"
+	echo "Core	-> found: $number_core_found/$number_core_total	| not found: $number_core_notfound"
+	echo "Other	-> found: $number_other_found/$number_other_total	| not found: $number_other_notfound"
 
+
+	if [ $number_core_notfound -gt 0 ]; then
+		echo ""
+		echo "Error: missing core file(s). A reinstallation is required (https://github.com/bashpack-project/bashpack?tab=readme-ov-file#quick-start)"
+	elif [ $number_other_notfound -gt 0 ]; then
+		echo ""
+		echo "Error: core file(s) are working, but some features are not working as expected. 'sudo bp -i' should solve the issue (if not, you can open an issue at https://github.com/bashpack-project/bashpack/issues)"
+	else
+		echo ""
+		echo "$NAME is working as expected !"
+	fi
 }
+
+
+
+
+check_files
+
+
 
 
 # Properly exit
 exit
+
+#EOF
