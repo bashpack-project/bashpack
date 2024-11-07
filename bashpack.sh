bb95f7a0884dad81dd4a6927e98f4c84e7c27623
ggtrd <geoffrey@wsl>
2024-11-07 23:14:33 +0100

    small fix

--- bashpack.sh
+++ bashpack.sh
@@ -158,7 +158,7 @@ fi
 set -e
 if [ "$(id -u)" != "0" ]; then
 	echo "$now error:   must be runned as root."
-	exit 1
+	exit
 fi
 
 
@@ -925,7 +925,7 @@ create_cli() {
 			cp "$archive_dir_tmp/config/$file_config" "$file_config"
 
 		else
-			display_info "$file_config already exists, copy new file while leaving current configured options."
+			display_info "$file_config already exists, install new file and insert current configured options."
 			install_new_config_file
 		fi
 		
